2e093b8a55aff545dec8a746e3c095064ee86ac6
Henry <henry@rovicomm.com>
2024-08-08 11:54:06 -0400

    Revert "readded check_reboot"
    This reverts commit 7f3a1413df9dd991d8648c9dc142704c406b6c16.

--- scripts/all_checks.py
+++ scripts/all_checks.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
-
+  
 import os
 import sys
-
-def check_reboot():
-    """Returns True if the computer has a pending reboot."""
-    return os.path.exists("/run/reboot-required")
 
 def main():
     if check_reboot():
