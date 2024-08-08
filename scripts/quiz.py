425188a3ce15cfb24373bf2fc962754096ca312a
Henry <henry@rovicomm.com>
2024-08-08 11:58:42 -0400

    Revert "init commit"
    This reverts commit 005b854143778ceee6035683dabec7e5ef93fb89.

--- scripts/quiz.py
+++
@@ -1,8 +0,0 @@
-def safe_division(numerator, denominator):
-    if denominator > 1:
-        result = numerator / denominator
-    else:
-        result = numerator / 1
-    return result
-
-    print(safe_division(5,0))
--- scripts/while.py
+++
@@ -1,6 +0,0 @@
-x =   0
-while x < 5:
-    print("Not there yet, x=" + str(x))
-    x = x + 1
-print("x=" + str(x))
-
