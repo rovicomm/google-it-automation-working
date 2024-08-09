7f485904e2df059b023554f2768f51eaf105c5a0
Henry <henry@rovicomm.com>
2024-08-09 10:43:47 -0400

    back on track

--- scripts/all_checks.py
+++ scripts/all_checks.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import shutil
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
@@ -18,9 +19,23 @@ def check_disk_full(disk, min_gb, min_percent):
         return True
     return False
 
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise."""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+
 def main():
-    if check_reboot():
-        print("Pending Reboot.")
+    checks=[
+        (check_reboot, "Pending Reboot"),
+        (check_root_full, "Root parition full"),
+        ]
+    everything_ok= True
+    for check, msg in checks:
+        if check():
+            print(msg)
+            everything_ok= False
+
+    if not everything_ok:
         sys.exit(1)
 
     print("Everything ok.")
