f268e4d9d1f6271a1d5431db274315df70f9b394
Henry <henry@rovicomm.com>
2024-08-09 10:09:15 -0400

    added local files

--- scripts/free_memory.py
+++ scripts/free_memory.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 def main():
+<<<<<<< HEAD
     """Checks if there's enough free memory in the computer."
+=======
+    print("Everything is ok.")
+>>>>>>> c5ac9b9 (Print everything is ok.)
 
 main()
